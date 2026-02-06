54631a83b3c546400c0e5afa0756b119c3e26818
jubayer17 <jubayer17@cse.pstu.ac.bd>
2026-02-06 18:50:13 +0600

    chore: remove client-side code and database schema files
    - Delete Prisma schema, migrations, and seed files
    - Remove job listing pages and components (featured-jobs, latest-jobs, job-details)
    - Remove job posting and application functionality
    - Delete related UI components and service layer files

--- client/prisma/migrations/20260127090941_added_applylink_to_job_schema/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Job" ADD COLUMN     "applyLink" TEXT NOT NULL DEFAULT '';
--- client/prisma/migrations/20260129045756_add_job_details/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Job" ADD COLUMN     "logo" TEXT,
-ADD COLUMN     "salaryMax" INTEGER,
-ADD COLUMN     "salaryMin" INTEGER,
-ADD COLUMN     "tags" TEXT[] DEFAULT ARRAY[]::TEXT[];
