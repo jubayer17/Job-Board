81622abb17a4c09ba7d8cb6b30233f0dc8bab96a
jubayer17 <jubayer17@cse.pstu.ac.bd>
2026-02-06 19:16:55 +0600

    feat(client): add Apollo provider for GraphQL integration
    Set up Apollo Client with HTTP link to local GraphQL server and InMemoryCache.
    This enables React components to query GraphQL data throughout the application.

---
+++ client/providers/apollo-provider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "http://localhost:4000/graphql",
+  }),
+
+  cache: new InMemoryCache(),
+});
+
+export default function ApolloWrapper({ children }: { children: React.ReactNode }) {
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+}
