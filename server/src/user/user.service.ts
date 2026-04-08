143ef500634ba194bd2c8f0175e6e408ddae95ee
jubayer17 <jubayer17@cse.pstu.ac.bd>
2026-04-08 23:54:05 +0600

    refactor(user): improve formatting and consistency in UserService methods

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -8,47 +8,46 @@ import { LoginInput } from './dto/login.input';
 
 @Injectable()
 export class UserService {
-    constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) {}
 
-    async login(data: LoginInput): Promise<User> {
-        const user = await this.prisma.user.findUnique({
-            where: { email: data.email },
-        });
+  async login(data: LoginInput): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: { email: data.email },
+    });
 
-        if (!user || !user.password) {
-            throw new Error('Invalid email or password');
-        }
-
-        const isPasswordValid = await bcrypt.compare(data.password, user.password);
-
-        if (!isPasswordValid) {
-            throw new Error('Invalid email or password');
-        }
-
-        return {
-            ...user,
-            name: user.name ?? '',
-            email: user.email ?? '',
-        };
+    if (!user || !user.password) {
+      throw new Error('Invalid email or password');
     }
 
-    async createUser(data: CreateUserInput): Promise<User> {
-        // Hash the password
-        const hashedPassword = await bcrypt.hash(data.password, 10);
+    const isPasswordValid = await bcrypt.compare(data.password, user.password);
 
-        const user = await this.prisma.user.create({
-            data: {
-                name: data.name,
-                email: data.email,
-                password: hashedPassword,
-            },
-        });
-
-        return {
-            ...user,
-            name: user.name ?? '',
-            email: user.email ?? '',
-
-        };
+    if (!isPasswordValid) {
+      throw new Error('Invalid email or password');
     }
+
+    return {
+      ...user,
+      name: user.name ?? '',
+      email: user.email ?? '',
+    };
+  }
+
+  async createUser(data: CreateUserInput): Promise<User> {
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        name: data.name,
+        email: data.email,
+        password: hashedPassword,
+      },
+    });
+
+    return {
+      ...user,
+      name: user.name ?? '',
+      email: user.email ?? '',
+    };
+  }
 }
