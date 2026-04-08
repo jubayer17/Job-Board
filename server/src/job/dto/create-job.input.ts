e6ccd1503fbc9df4f99c6a6a9e50015f01a568db
jubayer17 <jubayer17@cse.pstu.ac.bd>
2026-04-08 23:53:06 +0600

    feat(job): expand CreateJobInput with additional fields for enhanced job details

--- server/src/job/dto/create-job.input.ts
+++ server/src/job/dto/create-job.input.ts
@@ -2,66 +2,120 @@ import { InputType, Field } from '@nestjs/graphql';
 
 @InputType()
 export class CreateJobInput {
-    @Field()
-    title: string;
+  @Field()
+  title: string;
 
-    @Field()
-    company: string;
+  @Field()
+  company: string;
 
-    @Field({ nullable: true })
-    location?: string;
+  @Field({ nullable: true })
+  location?: string;
 
-    @Field()
-    type: string;
+  @Field()
+  type: string;
 
-    @Field()
-    description: string;
+  @Field()
+  description: string;
 
-    @Field(() => String, { nullable: true })
-    salary?: string;
+  @Field(() => String, { nullable: true })
+  salary?: string;
 
-    @Field(() => [String], { nullable: true })
-    tags?: string[];
+  @Field(() => [String], { nullable: true })
+  tags?: string[];
 
-    @Field({ nullable: true })
-    deadline?: Date;
+  @Field({ nullable: true })
+  deadline?: Date;
 
-    @Field({ nullable: true })
-    vacancies?: number;
+  @Field({ nullable: true })
+  vacancies?: number;
 
-    @Field({ nullable: true })
-    experience?: string;
+  @Field({ nullable: true })
+  experience?: string;
 
-    @Field({ nullable: true })
-    education?: string;
+  @Field({ nullable: true })
+  education?: string;
 
-    @Field({ nullable: true })
-    workplace?: string;
+  @Field({ nullable: true })
+  workplace?: string;
 
-    @Field({ nullable: true })
-    jobContext?: string;
+  @Field({ nullable: true })
+  jobContext?: string;
 
-    @Field({ nullable: true })
-    gender?: string;
+  @Field({ nullable: true })
+  gender?: string;
 
-    @Field({ nullable: true })
-    applyLink?: string;
+  @Field({ nullable: true })
+  applyLink?: string;
 
-    @Field({ nullable: true })
-    logo?: string;
+  @Field({ nullable: true })
+  logo?: string;
 
-    @Field({ nullable: true })
-    logoPublicId?: string;
+  @Field({ nullable: true })
+  logoPublicId?: string;
 
-    @Field({ nullable: true })
-    logoOriginalName?: string;
+  @Field({ nullable: true })
+  logoOriginalName?: string;
 
-    @Field({ nullable: true })
-    logoUploadedAt?: Date;
+  @Field({ nullable: true })
+  logoUploadedAt?: Date;
 
-    @Field()
-    companyId: string;
+  @Field({ nullable: true })
+  roleCategory?: string;
 
-    @Field()
-    locationId: string;
+  @Field({ nullable: true })
+  workModel?: string;
+
+  @Field({ nullable: true })
+  companyCulture?: string;
+
+  @Field({ nullable: true })
+  careerGrowth?: string;
+
+  @Field({ nullable: true })
+  interviewProcess?: string;
+
+  @Field({ nullable: true })
+  industrySpecificRequirements?: string;
+
+  @Field({ nullable: true })
+  compensationCurrency?: string;
+
+  @Field({ nullable: true })
+  payPeriod?: string;
+
+  @Field({ nullable: true })
+  bonusDetails?: string;
+
+  @Field({ nullable: true })
+  equityDetails?: string;
+
+  @Field(() => [String], { nullable: true })
+  skillsAdvanced?: string[];
+
+  @Field(() => [String], { nullable: true })
+  skillsIntermediate?: string[];
+
+  @Field(() => [String], { nullable: true })
+  skillsFoundational?: string[];
+
+  @Field(() => [String], { nullable: true })
+  softSkills?: string[];
+
+  @Field(() => [String], { nullable: true })
+  certifications?: string[];
+
+  @Field({ nullable: true })
+  experienceMinYears?: number;
+
+  @Field({ nullable: true })
+  experienceMaxYears?: number;
+
+  @Field({ nullable: true })
+  travelRequirement?: string;
+
+  @Field()
+  companyId: string;
+
+  @Field()
+  locationId: string;
 }
