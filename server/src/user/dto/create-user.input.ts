b126982c3a9782144141e402ea1ed7c8e9219d16
jubayer17 <jubayer17@cse.pstu.ac.bd>
2026-04-08 23:54:12 +0600

    refactor(user): adjust indentation for consistency in CreateUserInput class

--- server/src/user/dto/create-user.input.ts
+++ server/src/user/dto/create-user.input.ts
@@ -3,12 +3,12 @@ import { InputType, Field } from '@nestjs/graphql';
 
 @InputType()
 export class CreateUserInput {
-    @Field()
-    name: string;
+  @Field()
+  name: string;
 
-    @Field()
-    email: string;
+  @Field()
+  email: string;
 
-    @Field()
-    password: string;
+  @Field()
+  password: string;
 }
