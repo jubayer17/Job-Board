c8f9c76066c5428d3e2640160159b7b28c587637
jubayer17 <jubayer17@cse.pstu.ac.bd>
2026-04-08 23:48:07 +0600

    feat(search): remove obsolete search index SQL script

--- server/add_search_index.sql
+++
@@ -1,2 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
-CREATE INDEX IF NOT EXISTS job_search_idx ON "Job" USING GIN (to_tsvector('english', coalesce(title, '') || ' ' || coalesce(company, '') || ' ' || coalesce(description, '') || ' ' || coalesce(location, '')));
