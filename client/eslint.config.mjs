b5409c7349cc213901e7bc265513e46987ea74a3
jubayer17 <jubayer17@cse.pstu.ac.bd>
2026-04-08 23:46:41 +0600

    refactor(eslint): simplify ESLint configuration and remove unused imports

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,16 +1,19 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import coreWebVitals from "eslint-config-next/core-web-vitals";
+import typescript from "eslint-config-next/typescript";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  {
+    ignores: ["jest.config.js"],
+  },
+  ...coreWebVitals,
+  ...typescript,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-require-imports": "off",
+      "react/no-unescaped-entities": "off",
+      "import/no-anonymous-default-export": "off",
+    },
+  },
 ];
-
-export default eslintConfig;
