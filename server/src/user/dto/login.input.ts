fc59fe10d0e91f683a2107963645082bc7bb35a0
jubayer17 <jubayer17@cse.pstu.ac.bd>
2026-04-08 23:54:20 +0600

    refactor(user): adjust indentation for consistency in LoginInput class

--- server/src/user/dto/login.input.ts
+++ server/src/user/dto/login.input.ts
@@ -2,9 +2,9 @@ import { InputType, Field } from '@nestjs/graphql';
 
 @InputType()
 export class LoginInput {
-    @Field()
-    email!: string;
+  @Field()
+  email!: string;
 
-    @Field()
-    password!: string;
+  @Field()
+  password!: string;
 }
